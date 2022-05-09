cf06142787ad7c6e215076f5ce7a789b21b4c8b2
mikhirurg <mikhail.a.ushakov@gmail.com>
2022-05-09 16:49:07 +0200

    Main activities

---
+++ app/src/main/java/io/github/textrecognisionsample/AccountCreate.java
@@ -0,0 +1,14 @@
+package io.github.textrecognisionsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccountCreate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_create);
+    }
+}
---
+++ app/src/main/java/io/github/textrecognisionsample/EditCoupon.java
@@ -0,0 +1,14 @@
+package io.github.textrecognisionsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditCoupon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_coupon);
+    }
+}
